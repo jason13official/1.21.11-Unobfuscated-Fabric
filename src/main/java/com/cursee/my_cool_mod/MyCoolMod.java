af1ee6225dd6eb1d9b97444c762939077daa98c4
jason13official <jason13official@gmail.com>
2025-12-11 10:15:25 -0500

    feat: proj files

---
+++ src/main/java/com/cursee/my_cool_mod/MyCoolMod.java
@@ -0,0 +1,15 @@
+package com.cursee.my_cool_mod;
+
+import com.cursee.my_cool_mod.impl.registry.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+public class MyCoolMod implements ModInitializer {
+
+  public static final String MOD_ID = "my_cool_mod";
+
+  @Override
+  public void onInitialize() {
+
+    // ModItems.registerItems();
+  }
+}
